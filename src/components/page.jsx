9b19aedb5474bc17ccf6e3914ad07faf88d13172
GUJUGIT <next39742@edu.nextkbh.dk>
2026-09-24 12:19:24 +0200

    oprettede test 5 og lavet test 4 og 5 til del A og B

--- src/components/page.jsx
+++ src/components/page.jsx
@@ -3,7 +3,8 @@ export default function TestPage() {
         <h1>Dette er en test</h1>
         <section><p>Dette er test nr 2</p></section>
         <section><p>Dette er test nr 3</p></section>
-
+        <section><p>Dette er test nr 4 - del A</p></section>
+        <section><p>Dette er test nr 5 - del B</p></section>
     </>
     );
 }
