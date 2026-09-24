99b8e4bcbc592c56b67352f72df7aef4481dbc71
GUJUGIT <next39742@edu.nextkbh.dk>
2026-09-24 11:19:32 +0200

    oprettet test nr 3

--- src/components/page.jsx
+++ src/components/page.jsx
@@ -2,6 +2,7 @@ export default function TestPage() {
     return (<>
         <h1>Dette er en test</h1>
         <section><p>Dette er test nr 2</p></section>
+        <section><p>Dette er test nr 3</p></section>
 
     </>
     );
